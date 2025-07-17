a47312efbb5a2aee036fd8d54c3ffbca1e6e408c
Mihir-11 <batra.singh.m@gmail.com>
2025-07-18 10:50:00 +1200

    Add files via upload

--- Assement_v1.py
+++ Assement_v1.py
@@ -4,7 +4,6 @@ Tycoon clicker game
 Written by Mihir Batra
 '''
 from tkinter import *
-from tkinter import ttk
 
 class Game:
     def __init__(self):
@@ -17,7 +16,19 @@ class Game:
         banner.grid(column = 0, row = 0, sticky = "WE", ipadx=20)
 
         button_play = Button(self.root, text = "Play", font = "arial 20 bold")
-        button_play.grid(column = 0, row = 1, pady = 20)
+        button_play.grid(column = 0, row = 1, pady = 10)
+        button_help = Button(self.root, text = "Help", font = "arial 20 bold")
+        button_help.grid(column = 0, row = 2, pady = 10)
+
+        #container for frames
+        self.container = Frame(self.root)
+        self.container.grid(sticky = "NSEW")
+
+        #dictionary to hold frames
+        #self.frames = {}
+        #self.frames["homepage"] = self.home_frame()
+        #self.frames["gamepage"] = self.game_frame()
+        #self.frames["helppage"] = self.help_frame()
 
 
 
