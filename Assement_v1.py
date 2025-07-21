d7da444ab6e8eddaedb294dc2d8fabf1da9f6090
Mihir-11 <batra.singh.m@gmail.com>
2025-07-22 09:47:56 +1200

    Add files via upload
    Can now switch between frames

--- Assement_v1.py
+++ Assement_v1.py
@@ -12,30 +12,51 @@ class Game:
         self.root.title("Clicker Game")
 
         #window design
-        banner = Label(self.root, text = "Clicker Game", font = "arial 30 bold")
-        banner.grid(column = 0, row = 0, sticky = "WE", ipadx=20)
-
-        button_play = Button(self.root, text = "Play", font = "arial 20 bold")
-        button_play.grid(column = 0, row = 1, pady = 10)
-        button_help = Button(self.root, text = "Help", font = "arial 20 bold")
-        button_help.grid(column = 0, row = 2, pady = 10)
+       
 
         #container for frames
         self.container = Frame(self.root)
         self.container.grid(sticky = "NSEW")
 
+        
         #dictionary to hold frames
-        #self.frames = {}
-        #self.frames["homepage"] = self.home_frame()
+        self.frames = {}
+        self.frames["homepage"] = self.home_frame()
+        self.frames["helppage"] = self.help_frame()
         #self.frames["gamepage"] = self.game_frame()
-        #self.frames["helppage"] = self.help_frame()
+        
+
+    def show_frame(self, name):
+        frame = self.frames[name]
+        frame.tkraise()
+
+    def home_frame(self):
+        frame = Frame(self.container)
+        frame.grid(row= 0, column = 0, sticky = "NSEW")
+
+        banner = Label(frame, text = "Clicker Game", font = "arial 30 bold")
+        banner.grid(column = 0, row = 0, sticky = "WE", ipadx=20)
+
+        button_play = Button(frame, text = "Play", font = "arial 20 bold")
+        button_play.grid(column = 0, row = 1, pady = 10)
+        button_help = Button(frame, text = "Help", font = "arial 20 bold", 
+                             command = lambda: self.show_frame("helppage"))
+        button_help.grid(column = 0, row = 2, pady = 10)
+        return frame
 
 
+    def help_frame(self):
+        frame = Frame(self.container)
+        frame.grid(row= 0, column = 0, sticky = "NSEW")
+
+        help = Label(frame, text = "Help", font = "arial 30 bold")
+        help.grid(column = 0, row = 0, sticky = "WE", ipadx=20)
+
+        return frame
 
 
-
-    
     def run(self):
+        self.show_frame("homepage")
         self.root.mainloop()
 
 app = Game()
