950b0797c2d6aeea3124526caa030eb9dc40e93a
Mihir-11 <batra.singh.m@gmail.com>
2025-07-25 10:49:11 +1200

    basic home page and help page complete

--- Assement_v1.py
+++ Assement_v1.py
@@ -59,6 +59,20 @@ class Game:
         banner_help = Label(frame, text = "Welcome to [Game name]!", font = "arial 30 bold")
         banner_help.grid(column = 0, row = 0, sticky = "WE", ipadx=20)
 
+        what_label = Label(frame, text = '''This game is a basic clicker game that works by you the user clicking on the[object] to earn[currency].
+        The aim of the game is to try and have more [currency] than other players and be top of the leaderboard!
+                           
+        To start off you can earn 1 [currency] per click on [object], however once you save up enough [currency] you can go to the store to purchase upgrades.
+        From the upgrade store you will be able to purchase upgrades that increase your amount of [currency] earned per click or you could purchase workers 
+        that provide you with passive income ''',
+                           font = "arial 12")
+        what_label.grid(column = 0, row = 1, sticky = "WE", ipadx=20)
+
+        back_button = Button(frame, text = "Back", font = "arial 20 bold",
+                             command = lambda: self.show_frame("homepage"))
+        back_button.grid(column = 0, row = 2, pady=10)
+
+
         return frame
     
     def game_frame(self):
@@ -67,7 +81,10 @@ class Game:
         frame.grid_columnconfigure(0, weight =1)
 
         banner_game = Label(frame, text = "Game", font = "arial 30 bold")
-        banner_game.grid(row = 0, column = 0,sticky = "WE", ipadx=20)
+        banner_game.grid(column = 0, row = 0, sticky = "WE", ipadx=20)
+
+        clicker = Button(frame, text = "click me", font = "arial 40 bold")
+        clicker.grid(column = 0, row = 1, pady = 30)
         
 
         return frame
